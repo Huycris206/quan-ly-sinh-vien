68ff8d5c27135ed4fb0983506e744aa2231b6fff
Phan Quang Huy <huycris567@gmail.com>
2026-04-18 19:44:00 +0700

    sql

--- sql/database.sql
+++ sql/database.sql
@@ -1,316 +1,221 @@
 USE [QuanLyHoSoSinhVien]
 GO
-/****** Object:  Table [dbo].[ChuongTrinhDaoTao]    Script Date: 4/16/2026 8:55:24 PM ******/
-SET ANSI_NULLS ON
-GO
-SET QUOTED_IDENTIFIER ON
-GO
-CREATE TABLE [dbo].[ChuongTrinhDaoTao](
-	[ChuyenNganhId] [uniqueidentifier] NOT NULL,
-	[MonHocId] [uniqueidentifier] NOT NULL,
-	[LoaiMon] [varchar](20) NULL,
-	[CreatedAt] [datetime2](7) NULL,
-PRIMARY KEY CLUSTERED 
-(
-	[ChuyenNganhId] ASC,
-	[MonHocId] ASC
-)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
+
+-- ==========================================
+-- 1. TẠO BẢNG
+-- ==========================================
+
+CREATE TABLE [dbo].[TAIKHOAN](
+	[ID] [uniqueidentifier] NOT NULL DEFAULT newid(),
+	[TENDANGNHAP] [varchar](100) NOT NULL,
+	[MATKHAU] [varchar](255) NOT NULL,
+	[VAITRO] [varchar](20) DEFAULT 'sinhvien', -- 'sinhvien', 'giangvien', 'quantri'
+	[ANHDAIDIEN] [nvarchar](500) NULL,
+	[NGAYTAO] [datetime2](7) DEFAULT getdate(),
+	[NGAYCAPNHAT] [datetime2](7) DEFAULT getdate(),
+	[DAXOA] [bit] DEFAULT 0,
+PRIMARY KEY CLUSTERED ([ID] ASC),
+UNIQUE NONCLUSTERED ([TENDANGNHAP] ASC)
 ) ON [PRIMARY]
 GO
-/****** Object:  Table [dbo].[ChuyenNganh]    Script Date: 4/16/2026 8:55:24 PM ******/
-SET ANSI_NULLS ON
-GO
-SET QUOTED_IDENTIFIER ON
-GO
-CREATE TABLE [dbo].[ChuyenNganh](
-	[Id] [uniqueidentifier] NOT NULL,
-	[NganhId] [uniqueidentifier] NOT NULL,
-	[Name] [nvarchar](100) NOT NULL,
-	[CreatedAt] [datetime2](7) NULL,
-	[UpdatedAt] [datetime2](7) NULL,
-	[IsDeleted] [bit] NULL,
-PRIMARY KEY CLUSTERED 
-(
-	[Id] ASC
-)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
+
+CREATE TABLE [dbo].[NGANH](
+	[ID] [uniqueidentifier] NOT NULL DEFAULT newid(),
+	[TENNGANH] [nvarchar](100) NOT NULL,
+	[NGAYTAO] [datetime2](7) DEFAULT getdate(),
+	[NGAYCAPNHAT] [datetime2](7) DEFAULT getdate(),
+	[DAXOA] [bit] DEFAULT 0,
+PRIMARY KEY CLUSTERED ([ID] ASC)
 ) ON [PRIMARY]
 GO
-/****** Object:  Table [dbo].[GiangVien]    Script Date: 4/16/2026 8:55:24 PM ******/
-SET ANSI_NULLS ON
-GO
-SET QUOTED_IDENTIFIER ON
-GO
-CREATE TABLE [dbo].[GiangVien](
-	[Id] [uniqueidentifier] NOT NULL,
-	[UserId] [uniqueidentifier] NOT NULL,
-	[Teacher_id] [varchar](20) NOT NULL,
-	[Full_name] [nvarchar](100) NOT NULL,
-	[Gender] [nvarchar](10) NULL,
-	[Birthday] [date] NULL,
-	[Phone] [varchar](15) NULL,
-	[CreatedAt] [datetime2](7) NULL,
-	[UpdatedAt] [datetime2](7) NULL,
-	[IsDeleted] [bit] NULL,
-PRIMARY KEY CLUSTERED 
-(
-	[Id] ASC
-)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY],
-UNIQUE NONCLUSTERED 
-(
-	[UserId] ASC
-)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY],
-UNIQUE NONCLUSTERED 
-(
-	[Teacher_id] ASC
-)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
+
+CREATE TABLE [dbo].[CHUYENNGANH](
+	[ID] [uniqueidentifier] NOT NULL DEFAULT newid(),
+	[NGANH_ID] [uniqueidentifier] NOT NULL,
+	[TENCHUYENNGANH] [nvarchar](100) NOT NULL,
+	[NGAYTAO] [datetime2](7) DEFAULT getdate(),
+	[NGAYCAPNHAT] [datetime2](7) DEFAULT getdate(),
+	[DAXOA] [bit] DEFAULT 0,
+PRIMARY KEY CLUSTERED ([ID] ASC)
 ) ON [PRIMARY]
 GO
-/****** Object:  Table [dbo].[KetQuaHocTap]    Script Date: 4/16/2026 8:55:24 PM ******/
-SET ANSI_NULLS ON
-GO
-SET QUOTED_IDENTIFIER ON
-GO
-CREATE TABLE [dbo].[KetQuaHocTap](
-	[SinhVienId] [uniqueidentifier] NOT NULL,
-	[LopHocPhanId] [uniqueidentifier] NOT NULL,
-	[DiemSo] [float] NULL,
-	[CreatedAt] [datetime2](7) NULL,
-	[UpdatedAt] [datetime2](7) NULL,
-PRIMARY KEY CLUSTERED 
-(
-	[SinhVienId] ASC,
-	[LopHocPhanId] ASC
-)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
+
+CREATE TABLE [dbo].[SINHVIEN](
+	[ID] [uniqueidentifier] NOT NULL DEFAULT newid(),
+	[TAIKHOAN_ID] [uniqueidentifier] NOT NULL,
+	[MASV] [varchar](20) NOT NULL,
+	[HOTEN] [nvarchar](100) NOT NULL,
+	[GIOITINH] [nvarchar](10) NULL,
+	[NGAYSINH] [date] NULL,
+	[SDT] [varchar](15) NULL,
+	[EMAIL] [varchar](100) NULL,
+	[CCCD] [varchar](20) NULL,
+	[QUEQUAN] [nvarchar](255) NULL,
+	[DIACHI] [nvarchar](255) NULL,
+	[KHOAHOC] [varchar](20) NULL,
+	[CHUYENNGANH_ID] [uniqueidentifier] NOT NULL,
+	[TRANGTHAI] [varchar](20) DEFAULT 'DangHoc', 
+	[NGAYTAO] [datetime2](7) DEFAULT getdate(),
+	[NGAYCAPNHAT] [datetime2](7) DEFAULT getdate(),
+	[DAXOA] [bit] DEFAULT 0,
+PRIMARY KEY CLUSTERED ([ID] ASC),
+UNIQUE NONCLUSTERED ([TAIKHOAN_ID] ASC),
+UNIQUE NONCLUSTERED ([MASV] ASC),
+UNIQUE NONCLUSTERED ([CCCD] ASC)
 ) ON [PRIMARY]
 GO
-/****** Object:  Table [dbo].[LopHocPhan]    Script Date: 4/16/2026 8:55:24 PM ******/
-SET ANSI_NULLS ON
-GO
-SET QUOTED_IDENTIFIER ON
-GO
-CREATE TABLE [dbo].[LopHocPhan](
-	[Id] [uniqueidentifier] NOT NULL,
-	[MaLop] [varchar](50) NOT NULL,
-	[MonHocId] [uniqueidentifier] NOT NULL,
-	[HocKy] [varchar](20) NOT NULL,
-	[Sv_max] [int] NULL,
-	[Status] [varchar](20) NULL,
-	[CreatedAt] [datetime2](7) NULL,
-	[UpdatedAt] [datetime2](7) NULL,
-	[IsDeleted] [bit] NULL,
-	[Teacher_id] [uniqueidentifier] NULL,
-PRIMARY KEY CLUSTERED 
-(
-	[Id] ASC
-)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
+
+CREATE TABLE [dbo].[GIANGVIEN](
+	[ID] [uniqueidentifier] NOT NULL DEFAULT newid(),
+	[TAIKHOAN_ID] [uniqueidentifier] NOT NULL,
+	[MAGV] [varchar](20) NOT NULL,
+	[HOTEN] [nvarchar](100) NOT NULL,
+	[GIOITINH] [nvarchar](10) NULL,
+	[NGAYSINH] [date] NULL,
+	[SDT] [varchar](15) NULL,
+	[NGAYTAO] [datetime2](7) DEFAULT getdate(),
+	[NGAYCAPNHAT] [datetime2](7) DEFAULT getdate(),
+	[DAXOA] [bit] DEFAULT 0,
+PRIMARY KEY CLUSTERED ([ID] ASC),
+UNIQUE NONCLUSTERED ([TAIKHOAN_ID] ASC),
+UNIQUE NONCLUSTERED ([MAGV] ASC)
 ) ON [PRIMARY]
 GO
-/****** Object:  Table [dbo].[MonHoc]    Script Date: 4/16/2026 8:55:24 PM ******/
-SET ANSI_NULLS ON
-GO
-SET QUOTED_IDENTIFIER ON
-GO
-CREATE TABLE [dbo].[MonHoc](
-	[Id] [uniqueidentifier] NOT NULL,
-	[Name] [nvarchar](100) NOT NULL,
-	[TinChi] [int] NOT NULL,
-	[CreatedAt] [datetime2](7) NULL,
-	[UpdatedAt] [datetime2](7) NULL,
-	[IsDeleted] [bit] NULL,
-PRIMARY KEY CLUSTERED 
-(
-	[Id] ASC
-)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
+
+CREATE TABLE [dbo].[MONHOC](
+	[ID] [uniqueidentifier] NOT NULL DEFAULT newid(),
+	[TENMONHOC] [nvarchar](100) NOT NULL,
+	[SOTINCHI] [int] NOT NULL,
+	[NGAYTAO] [datetime2](7) DEFAULT getdate(),
+	[NGAYCAPNHAT] [datetime2](7) DEFAULT getdate(),
+	[DAXOA] [bit] DEFAULT 0,
+PRIMARY KEY CLUSTERED ([ID] ASC)
 ) ON [PRIMARY]
 GO
-/****** Object:  Table [dbo].[Nganh]    Script Date: 4/16/2026 8:55:24 PM ******/
-SET ANSI_NULLS ON
-GO
-SET QUOTED_IDENTIFIER ON
-GO
-CREATE TABLE [dbo].[Nganh](
-	[Id] [uniqueidentifier] NOT NULL,
-	[Name] [nvarchar](100) NOT NULL,
-	[CreatedAt] [datetime2](7) NULL,
-	[UpdatedAt] [datetime2](7) NULL,
-	[IsDeleted] [bit] NULL,
-PRIMARY KEY CLUSTERED 
-(
-	[Id] ASC
-)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
+
+CREATE TABLE [dbo].[CHUONGTRINHDAOTAO](
+	[CHUYENNGANH_ID] [uniqueidentifier] NOT NULL,
+	[MONHOC_ID] [uniqueidentifier] NOT NULL,
+	[LOAIMON] [varchar](20) DEFAULT 'BatBuoc',
+	[NGAYTAO] [datetime2](7) DEFAULT getdate(),
+PRIMARY KEY CLUSTERED ([CHUYENNGANH_ID] ASC, [MONHOC_ID] ASC)
 ) ON [PRIMARY]
 GO
-/****** Object:  Table [dbo].[SinhVien]    Script Date: 4/16/2026 8:55:24 PM ******/
-SET ANSI_NULLS ON
-GO
-SET QUOTED_IDENTIFIER ON
-GO
-CREATE TABLE [dbo].[SinhVien](
-	[Id] [uniqueidentifier] NOT NULL,
-	[UserId] [uniqueidentifier] NOT NULL,
-	[Student_id] [varchar](20) NOT NULL,
-	[Full_name] [nvarchar](100) NOT NULL,
-	[Gender] [nvarchar](10) NULL,
-	[Birthday] [date] NULL,
-	[Phone] [varchar](15) NULL,
-	[ChuyenNganhId] [uniqueidentifier] NOT NULL,
-	[TrangThai] [varchar](20) NULL,
-	[CreatedAt] [datetime2](7) NULL,
-	[UpdatedAt] [datetime2](7) NULL,
-	[IsDeleted] [bit] NULL,
-PRIMARY KEY CLUSTERED 
-(
-	[Id] ASC
-)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY],
-UNIQUE NONCLUSTERED 
-(
-	[UserId] ASC
-)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY],
-UNIQUE NONCLUSTERED 
-(
-	[Student_id] ASC
-)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
+
+CREATE TABLE [dbo].[LOPHOCPHAN](
+	[ID] [uniqueidentifier] NOT NULL DEFAULT newid(),
+	[MALOP] [varchar](50) NOT NULL,
+	[MONHOC_ID] [uniqueidentifier] NOT NULL,
+	[GIANGVIEN_ID] [uniqueidentifier] NULL,
+	[HOCKY] [varchar](20) NOT NULL,
+	[SISO_TOIDA] [int] DEFAULT 70,
+	[TRANGTHAI] [varchar](20) DEFAULT 'Mo',
+	[NGAYTAO] [datetime2](7) DEFAULT getdate(),
+	[NGAYCAPNHAT] [datetime2](7) DEFAULT getdate(),
+	[DAXOA] [bit] DEFAULT 0,
+PRIMARY KEY CLUSTERED ([ID] ASC)
 ) ON [PRIMARY]
 GO
-/****** Object:  Table [dbo].[Users]    Script Date: 4/16/2026 8:55:24 PM ******/
-SET ANSI_NULLS ON
-GO
-SET QUOTED_IDENTIFIER ON
-GO
-CREATE TABLE [dbo].[Users](
-	[Id] [uniqueidentifier] NOT NULL,
-	[Username] [varchar](100) NOT NULL,
-	[Password_hash] [varchar](255) NOT NULL,
-	[Role] [varchar](20) NULL,
-	[Avatar] [nvarchar](500) NULL,
-	[CreatedAt] [datetime2](7) NULL,
-	[UpdatedAt] [datetime2](7) NULL,
-	[IsDeleted] [bit] NULL,
-PRIMARY KEY CLUSTERED 
-(
-	[Id] ASC
-)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY],
-UNIQUE NONCLUSTERED 
-(
-	[Username] ASC
-)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
+
+CREATE TABLE [dbo].[KETQUAHOCTAP](
+	[SINHVIEN_ID] [uniqueidentifier] NOT NULL,
+	[LOPHOCPHAN_ID] [uniqueidentifier] NOT NULL,
+	[DIEMCHUYENCAN] [float] NULL,
+	[DIEMGIUAKY] [float] NULL,
+	[DIEMCUOIKY] [float] NULL,
+	[DIEMTONGKET] [float] NULL,
+	[DIEMHECHU] [varchar](5) NULL,
+	[NGAYTAO] [datetime2](7) DEFAULT getdate(),
+	[NGAYCAPNHAT] [datetime2](7) DEFAULT getdate(),
+PRIMARY KEY CLUSTERED ([SINHVIEN_ID] ASC, [LOPHOCPHAN_ID] ASC)
 ) ON [PRIMARY]
 GO
-ALTER TABLE [dbo].[ChuongTrinhDaoTao] ADD  DEFAULT ('BatBuoc') FOR [LoaiMon]
+CREATE TABLE [dbo].[MONTIENQUYET](
+	[MONHOC_ID] [uniqueidentifier] NOT NULL,
+	[MONHOC_TRUOC_ID] [uniqueidentifier] NOT NULL,
+	[LOAIDIEUKIEN] [varchar](20) DEFAULT 'TienQuyet', -- 'TienQuyet' (Phải qua môn), 'HocTruoc' (Chỉ cần từng học)
+PRIMARY KEY CLUSTERED ([MONHOC_ID] ASC, [MONHOC_TRUOC_ID] ASC),
+FOREIGN KEY([MONHOC_ID]) REFERENCES [dbo].[MONHOC] ([ID]),
+FOREIGN KEY([MONHOC_TRUOC_ID]) REFERENCES [dbo].[MONHOC] ([ID])
+) ON [PRIMARY]
 GO
-ALTER TABLE [dbo].[ChuongTrinhDaoTao] ADD  DEFAULT (getdate()) FOR [CreatedAt]
+
+-- 2. Bảng Lịch học (Giải quyết thời khóa biểu)
+CREATE TABLE [dbo].[LICHHOC](
+	[ID] [uniqueidentifier] NOT NULL DEFAULT newid(),
+	[LOPHOCPHAN_ID] [uniqueidentifier] NOT NULL,
+	[THU] [int] NOT NULL, -- Từ 2 đến 8 (Chủ nhật)
+	[TIET_BATDAU] [int] NOT NULL, -- Tiết 1 đến 12
+	[TIET_KETTHUC] [int] NOT NULL,
+	[PHONGHOC] [varchar](50) NOT NULL,
+PRIMARY KEY CLUSTERED ([ID] ASC),
+FOREIGN KEY([LOPHOCPHAN_ID]) REFERENCES [dbo].[LOPHOCPHAN] ([ID]),
+CHECK ([THU] >= 2 AND [THU] <= 8),
+CHECK ([TIET_BATDAU] > 0 AND [TIET_KETTHUC] >= [TIET_BATDAU])
+) ON [PRIMARY]
 GO
-ALTER TABLE [dbo].[ChuyenNganh] ADD  DEFAULT (newid()) FOR [Id]
+
+-- 3. Cập nhật bảng Kết quả học tập (Để quản lý trạng thái lúc đăng ký)
+ALTER TABLE [dbo].[KETQUAHOCTAP]
+ADD [TRANGTHAI_DANGKY] [varchar](20) DEFAULT 'ThanhCong';
 GO
-ALTER TABLE [dbo].[ChuyenNganh] ADD  DEFAULT (getdate()) FOR [CreatedAt]
+
+ALTER TABLE [dbo].[KETQUAHOCTAP] 
+ADD CHECK ([TRANGTHAI_DANGKY] IN ('ThanhCong', 'ChoDuyet', 'DaHuy'));
 GO
-ALTER TABLE [dbo].[ChuyenNganh] ADD  DEFAULT (getdate()) FOR [UpdatedAt]
+
+
+
+
+
+
+-- ==========================================
+-- 2. THIẾT LẬP KHÓA NGOẠI (FOREIGN KEYS)
+-- ==========================================
+
+ALTER TABLE [dbo].[CHUYENNGANH] ADD FOREIGN KEY([NGANH_ID]) REFERENCES [dbo].[NGANH] ([ID])
 GO
-ALTER TABLE [dbo].[ChuyenNganh] ADD  DEFAULT ((0)) FOR [IsDeleted]
+ALTER TABLE [dbo].[SINHVIEN] ADD FOREIGN KEY([TAIKHOAN_ID]) REFERENCES [dbo].[TAIKHOAN] ([ID])
 GO
-ALTER TABLE [dbo].[GiangVien] ADD  DEFAULT (newid()) FOR [Id]
+ALTER TABLE [dbo].[SINHVIEN] ADD FOREIGN KEY([CHUYENNGANH_ID]) REFERENCES [dbo].[CHUYENNGANH] ([ID])
 GO
-ALTER TABLE [dbo].[GiangVien] ADD  DEFAULT (getdate()) FOR [CreatedAt]
+ALTER TABLE [dbo].[GIANGVIEN] ADD FOREIGN KEY([TAIKHOAN_ID]) REFERENCES [dbo].[TAIKHOAN] ([ID])
 GO
-ALTER TABLE [dbo].[GiangVien] ADD  DEFAULT (getdate()) FOR [UpdatedAt]
+ALTER TABLE [dbo].[CHUONGTRINHDAOTAO] ADD FOREIGN KEY([CHUYENNGANH_ID]) REFERENCES [dbo].[CHUYENNGANH] ([ID])
 GO
-ALTER TABLE [dbo].[GiangVien] ADD  DEFAULT ((0)) FOR [IsDeleted]
+ALTER TABLE [dbo].[CHUONGTRINHDAOTAO] ADD FOREIGN KEY([MONHOC_ID]) REFERENCES [dbo].[MONHOC] ([ID])
 GO
-ALTER TABLE [dbo].[KetQuaHocTap] ADD  DEFAULT ((0.0)) FOR [DiemSo]
+ALTER TABLE [dbo].[LOPHOCPHAN] ADD FOREIGN KEY([MONHOC_ID]) REFERENCES [dbo].[MONHOC] ([ID])
 GO
-ALTER TABLE [dbo].[KetQuaHocTap] ADD  DEFAULT (getdate()) FOR [CreatedAt]
+ALTER TABLE [dbo].[LOPHOCPHAN] ADD FOREIGN KEY([GIANGVIEN_ID]) REFERENCES [dbo].[GIANGVIEN] ([ID])
 GO
-ALTER TABLE [dbo].[KetQuaHocTap] ADD  DEFAULT (getdate()) FOR [UpdatedAt]
+ALTER TABLE [dbo].[KETQUAHOCTAP] ADD FOREIGN KEY([SINHVIEN_ID]) REFERENCES [dbo].[SINHVIEN] ([ID])
 GO
-ALTER TABLE [dbo].[LopHocPhan] ADD  DEFAULT (newid()) FOR [Id]
+ALTER TABLE [dbo].[KETQUAHOCTAP] ADD FOREIGN KEY([LOPHOCPHAN_ID]) REFERENCES [dbo].[LOPHOCPHAN] ([ID])
 GO
-ALTER TABLE [dbo].[LopHocPhan] ADD  DEFAULT ((70)) FOR [Sv_max]
+
+
+-- ==========================================
+-- 3. THIẾT LẬP RÀNG BUỘC KIỂM TRA (CHECK CONSTRAINTS)
+-- ==========================================
+
+ALTER TABLE [dbo].[TAIKHOAN] ADD CHECK ([VAITRO] IN ('sinhvien', 'giangvien', 'quantri'))
 GO
-ALTER TABLE [dbo].[LopHocPhan] ADD  DEFAULT ('Mo') FOR [Status]
+ALTER TABLE [dbo].[SINHVIEN] ADD CHECK ([GIOITINH] IN (N'Nam', N'Nữ', N'Khác'))
 GO
-ALTER TABLE [dbo].[LopHocPhan] ADD  DEFAULT (getdate()) FOR [CreatedAt]
+ALTER TABLE [dbo].[SINHVIEN] ADD CHECK ([TRANGTHAI] IN ('DangHoc', 'BaoLuu', 'ThoiHoc', 'TotNghiep'))
 GO
-ALTER TABLE [dbo].[LopHocPhan] ADD  DEFAULT (getdate()) FOR [UpdatedAt]
+ALTER TABLE [dbo].[GIANGVIEN] ADD CHECK ([GIOITINH] IN (N'Nam', N'Nữ', N'Khác'))
 GO
-ALTER TABLE [dbo].[LopHocPhan] ADD  DEFAULT ((0)) FOR [IsDeleted]
+ALTER TABLE [dbo].[CHUONGTRINHDAOTAO] ADD CHECK ([LOAIMON] IN ('BatBuoc', 'TuChon'))
 GO
-ALTER TABLE [dbo].[MonHoc] ADD  DEFAULT (newid()) FOR [Id]
+ALTER TABLE [dbo].[LOPHOCPHAN] ADD CHECK ([TRANGTHAI] IN ('Mo', 'Dong', 'DangHoc', 'KetThuc', 'Huy'))
 GO
-ALTER TABLE [dbo].[MonHoc] ADD  DEFAULT (getdate()) FOR [CreatedAt]
+ALTER TABLE [dbo].[KETQUAHOCTAP] ADD CHECK ([DIEMCHUYENCAN] >= 0.0 AND [DIEMCHUYENCAN] <= 10.0)
 GO
-ALTER TABLE [dbo].[MonHoc] ADD  DEFAULT (getdate()) FOR [UpdatedAt]
+ALTER TABLE [dbo].[KETQUAHOCTAP] ADD CHECK ([DIEMGIUAKY] >= 0.0 AND [DIEMGIUAKY] <= 10.0)
 GO
-ALTER TABLE [dbo].[MonHoc] ADD  DEFAULT ((0)) FOR [IsDeleted]
+ALTER TABLE [dbo].[KETQUAHOCTAP] ADD CHECK ([DIEMCUOIKY] >= 0.0 AND [DIEMCUOIKY] <= 10.0)
 GO
-ALTER TABLE [dbo].[Nganh] ADD  DEFAULT (newid()) FOR [Id]
-GO
-ALTER TABLE [dbo].[Nganh] ADD  DEFAULT (getdate()) FOR [CreatedAt]
-GO
-ALTER TABLE [dbo].[Nganh] ADD  DEFAULT (getdate()) FOR [UpdatedAt]
-GO
-ALTER TABLE [dbo].[Nganh] ADD  DEFAULT ((0)) FOR [IsDeleted]
-GO
-ALTER TABLE [dbo].[SinhVien] ADD  DEFAULT (newid()) FOR [Id]
-GO
-ALTER TABLE [dbo].[SinhVien] ADD  DEFAULT ('DangHoc') FOR [TrangThai]
-GO
-ALTER TABLE [dbo].[SinhVien] ADD  DEFAULT (getdate()) FOR [CreatedAt]
-GO
-ALTER TABLE [dbo].[SinhVien] ADD  DEFAULT (getdate()) FOR [UpdatedAt]
-GO
-ALTER TABLE [dbo].[SinhVien] ADD  DEFAULT ((0)) FOR [IsDeleted]
-GO
-ALTER TABLE [dbo].[Users] ADD  DEFAULT (newid()) FOR [Id]
-GO
-ALTER TABLE [dbo].[Users] ADD  DEFAULT ('student') FOR [Role]
-GO
-ALTER TABLE [dbo].[Users] ADD  DEFAULT (getdate()) FOR [CreatedAt]
-GO
-ALTER TABLE [dbo].[Users] ADD  DEFAULT (getdate()) FOR [UpdatedAt]
-GO
-ALTER TABLE [dbo].[Users] ADD  DEFAULT ((0)) FOR [IsDeleted]
-GO
-ALTER TABLE [dbo].[ChuongTrinhDaoTao]  WITH CHECK ADD FOREIGN KEY([ChuyenNganhId])
-REFERENCES [dbo].[ChuyenNganh] ([Id])
-GO
-ALTER TABLE [dbo].[ChuongTrinhDaoTao]  WITH CHECK ADD FOREIGN KEY([MonHocId])
-REFERENCES [dbo].[MonHoc] ([Id])
-GO
-ALTER TABLE [dbo].[ChuyenNganh]  WITH CHECK ADD FOREIGN KEY([NganhId])
-REFERENCES [dbo].[Nganh] ([Id])
-GO
-ALTER TABLE [dbo].[KetQuaHocTap]  WITH CHECK ADD FOREIGN KEY([LopHocPhanId])
-REFERENCES [dbo].[LopHocPhan] ([Id])
-GO
-ALTER TABLE [dbo].[KetQuaHocTap]  WITH CHECK ADD FOREIGN KEY([SinhVienId])
-REFERENCES [dbo].[SinhVien] ([Id])
-GO
-ALTER TABLE [dbo].[LopHocPhan]  WITH CHECK ADD FOREIGN KEY([Teacher_id])
-REFERENCES [dbo].[GiangVien] ([Id])
-GO
-ALTER TABLE [dbo].[LopHocPhan]  WITH CHECK ADD FOREIGN KEY([MonHocId])
-REFERENCES [dbo].[MonHoc] ([Id])
-GO
-ALTER TABLE [dbo].[SinhVien]  WITH CHECK ADD FOREIGN KEY([ChuyenNganhId])
-REFERENCES [dbo].[ChuyenNganh] ([Id])
-GO
-ALTER TABLE [dbo].[SinhVien]  WITH CHECK ADD FOREIGN KEY([UserId])
-REFERENCES [dbo].[Users] ([Id])
-GO
-ALTER TABLE [dbo].[ChuongTrinhDaoTao]  WITH CHECK ADD CHECK  (([LoaiMon]='TuChon' OR [LoaiMon]='BatBuoc'))
-GO
-ALTER TABLE [dbo].[GiangVien]  WITH CHECK ADD CHECK  (([Gender]=N'Nam' OR [Gender]=N'Nữ' OR [Gender]=N'Khác'))
-GO
-ALTER TABLE [dbo].[KetQuaHocTap]  WITH CHECK ADD CHECK  (([DiemSo]>=(0.0) AND [DiemSo]<=(10.0)))
-GO
-ALTER TABLE [dbo].[LopHocPhan]  WITH CHECK ADD CHECK  (([Status]='Huy' OR [Status]='KetThuc' OR [Status]='DangHoc' OR [Status]='Dong' OR [Status]='Mo'))
-GO
-ALTER TABLE [dbo].[SinhVien]  WITH CHECK ADD CHECK  (([Gender]=N'Nam' OR [Gender]=N'Nữ' OR [Gender]=N'Khác'))
-GO
-ALTER TABLE [dbo].[SinhVien]  WITH CHECK ADD CHECK  (([TrangThai]='TotNghiep' OR [TrangThai]='ThoiHoc' OR [TrangThai]='BaoLuu' OR [TrangThai]='DangHoc'))
-GO
-ALTER TABLE [dbo].[Users]  WITH CHECK ADD CHECK  (([Role]='student' OR [Role]='admin' OR [Role]='teacher'))
-GO
-ALTER TABLE [dbo].[GiangVien]  WITH CHECK ADD FOREIGN KEY([UserId])
-REFERENCES [dbo].[Users] ([Id])
+ALTER TABLE [dbo].[KETQUAHOCTAP] ADD CHECK ([DIEMTONGKET] >= 0.0 AND [DIEMTONGKET] <= 10.0)
 GO

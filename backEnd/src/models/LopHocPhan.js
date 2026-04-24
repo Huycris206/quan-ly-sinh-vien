8027f19c7f7966c290cb9a5b1b67c190ad2d6178
Duyen_cute <duyenchuong35@gmail.com>
2026-04-24 20:28:08 +0700

    feat: setup tailwind css cho frontend

--- backEnd/src/models/LopHocPhan.js
+++ backEnd/src/models/LopHocPhan.js
@@ -1,26 +1,11 @@
 export default (sequelize, DataTypes) => {
     return sequelize.define('LopHocPhan', {
-<<<<<<< HEAD
-        Id: { type: DataTypes.UUID, defaultValue: DataTypes.UUIDV4, primaryKey: true },
-        MaLop: { type: DataTypes.STRING(50), allowNull: false },
-        Teacher_Id: { type: DataTypes.STRING(20), allowNull: false },
-        MonHocId: { type: DataTypes.UUID, allowNull: false },
-        HocKy: { type: DataTypes.STRING(20), allowNull: false },
-        Sv_max: { type: DataTypes.INTEGER, defaultValue: 70 },
-        Status: { 
-            type: DataTypes.STRING(20), 
-            defaultValue: 'Mo',
-            validate: { isIn: [['Huy', 'KetThuc', 'DangHoc', 'Dong', 'Mo']] }
-        },
-        IsDeleted: { type: DataTypes.BOOLEAN, defaultValue: false }
-=======
         ID: { type: DataTypes.UUID, defaultValue: DataTypes.UUIDV4, primaryKey: true },
         MALOP: { type: DataTypes.STRING(50), allowNull: false },
         HOCKY: { type: DataTypes.STRING(20), allowNull: false },
         SISO_TOIDA: { type: DataTypes.INTEGER, defaultValue: 70 },
         TRANGTHAI: { type: DataTypes.STRING(20), defaultValue: 'Mo' },
         DAXOA: { type: DataTypes.BOOLEAN, defaultValue: false }
->>>>>>> 49afb495f1df81dca20a5c77b96b6e407bc6c882
     }, { 
         tableName: 'LOPHOCPHAN', timestamps: true, createdAt: 'NGAYTAO', updatedAt: 'NGAYCAPNHAT' 
     });

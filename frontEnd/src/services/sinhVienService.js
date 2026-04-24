184be00e6fc915d3e39eb88699be7e42c9345c92
Duyen_cute <duyenchuong35@gmail.com>
2026-04-24 23:45:37 +0700

    feat: connect frontend to backend

--- frontEnd/src/services/sinhVienService.js
+++ frontEnd/src/services/sinhVienService.js
@@ -1,8 +1,35 @@
-import axios from 'axios'
+const BASE_URL = 'http://localhost:5001/api';
 
-const API_URL = 'http://localhost:5001/api'
+export const getAllSinhVien = () => {
+  return fetch(`${BASE_URL}/sinhvien`)
+    .then(res => res.json())
+    .then(json => json.data);
+};
 
-export const getAllSinhVien = async () => {
-  const response = await axios.get(`${API_URL}/sinhvien`)
-  return response.data.data
-}
+export const getSinhVienById = (id) => {
+  return fetch(`${BASE_URL}/sinhvien/${id}`)
+    .then(res => res.json())
+    .then(json => json.data);
+};
+
+export const createSinhVien = (data) => {
+  return fetch(`${BASE_URL}/sinhvien`, {
+    method: 'POST',
+    headers: { 'Content-Type': 'application/json' },
+    body: JSON.stringify(data)
+  }).then(res => res.json());
+};
+
+export const updateSinhVien = (id, data) => {
+  return fetch(`${BASE_URL}/sinhvien/${id}`, {
+    method: 'PUT',
+    headers: { 'Content-Type': 'application/json' },
+    body: JSON.stringify(data)
+  }).then(res => res.json());
+};
+
+export const deleteSinhVien = (id) => {
+  return fetch(`${BASE_URL}/sinhvien/${id}`, {
+    method: 'DELETE'
+  }).then(res => res.json());
+};
